c5f2cd0bfb1497553411302d9062936c0c6e2e5c
jisungbin <ji@sungb.in>
2022-06-23 16:00:40 +0900

    Set DI for PublicApiRepository - Add RepositoryModule for PublicApiRepository - Add UseCaseModule for PublicApiEntriesFetchUseCase

---
+++ presentation/src/main/kotlin/land/sungbin/apilibrary/di/RepositoryModule.kt
@@ -0,0 +1,24 @@
+/*
+ * ApiLibrary © 2022 Ji Sungbin. all rights reserved.
+ * ApiLibrary license is under the MIT.
+ *
+ * [RepositoryModule.kt] created by Ji Sungbin on 22. 6. 23. 오후 2:40
+ *
+ * Please see: https://github.com/jisungbin/ApiLibrary/blob/main/LICENSE.
+ */
+
+package land.sungbin.apilibrary.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import land.sungbin.apilibrary.data.repository.PublicApiRepositoryImpl
+import land.sungbin.apilibrary.domain.repository.PublicApiRepository
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object RepositoryModule {
+    @Provides
+    fun provideRepository(): PublicApiRepository = PublicApiRepositoryImpl()
+}
