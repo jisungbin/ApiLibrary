11a6beb69ac700db7fb9ffc731c8bed9091b2809
jisungbin <ji@sungb.in>
2022-06-23 11:42:40 +0900

    Add testing example at domain layer

---
+++ domain/src/test/kotlin/land/sungbin/apilibrary/domain/ExampleUnitTest.kt
@@ -0,0 +1,20 @@
+/*
+ * ApiLibrary © 2022 Ji Sungbin. all rights reserved.
+ * ApiLibrary license is under the MIT.
+ *
+ * [ExampleUnitTest.kt] created by Ji Sungbin on 22. 6. 23. 오전 10:37
+ *
+ * Please see: https://github.com/jisungbin/ApiLibrary/blob/main/LICENSE.
+ */
+
+package land.sungbin.apilibrary.domain
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
